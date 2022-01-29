f9c227726a18846020184871e9b25607e0cdd2b5
GuruVijay23 <vijayaguru.23@gmail.com>
2022-01-30 02:13:11 +0530

    Committing TestMethods

---
+++ src/test/java/com/cucumber/testmethods/TestMethods.java
@@ -0,0 +1,10 @@
+package com.cucumber.testmethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestMethods {
+	public WebDriver driver;
+	public WebDriverWait wait;
+
+}
