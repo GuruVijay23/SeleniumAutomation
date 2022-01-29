03985317293d62f20b57949cc292566eda281bf0
GuruVijay23 <vijayaguru.23@gmail.com>
2022-01-30 02:24:19 +0530

    Committing new Methods

--- src/test/java/com/cucumber/testmethods/TestMethods.java
+++ src/test/java/com/cucumber/testmethods/TestMethods.java
@@ -6,5 +6,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class TestMethods {
 	public WebDriver driver;
 	public WebDriverWait wait;
+	
+	public void waitTime(int sec) {
+			try {
+				Thread.sleep(sec);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
 
 }
