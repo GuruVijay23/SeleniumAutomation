7d9cb12f8cc20ac90ffcec9cf80496983bcef0d5
GuruVijay23 <vijayaguru.23@gmail.com>
2022-01-30 18:22:21 +0530

    Committing new methods

--- src/test/java/com/cucumber/testmethods/TestMethods.java
+++ src/test/java/com/cucumber/testmethods/TestMethods.java
@@ -1,5 +1,6 @@
 package com.cucumber.testmethods;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -7,12 +8,17 @@ public class TestMethods {
 	public WebDriver driver;
 	public WebDriverWait wait;
 	
-	public void waitTime(int sec) {
+	public void waitTime(int milliseconds) {
 			try {
-				Thread.sleep(sec);
+				Thread.sleep(milliseconds);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 	}
-
+	
+	public void scrollPage(int x,int y) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(" + x + "," + y + ")", "");
+	}
+	
 }
